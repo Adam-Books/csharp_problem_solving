2a7d3742e3e356fe11cdfdcdd3d06b8b5fffb5fb
Adam Mohammed <66003360+Adam-Books@users.noreply.github.com>
2022-01-27 10:06:40 +0200

    Create Multiply.cs

---
+++ codewars/8-kyu/Multiply.cs
@@ -0,0 +1,17 @@
+/*
+Title:
+  Multiply
+  
+Description:
+  This code does not execute properly. Try to figure out why.
+
+Link:
+  https://www.codewars.com/kata/50654ddff44f800200000004
+  
+*/
+
+// Solutions:
+// Solution 1:
+
+
+
