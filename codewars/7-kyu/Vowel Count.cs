1fa43c60747058c799bc22553376ad348ec6c242
Adam Mohammed <66003360+Adam-Books@users.noreply.github.com>
2022-02-01 08:43:51 +0200

    Vowel Count - C# Solution

--- codewars/7-kyu/Vowel Count.cs
+++ codewars/7-kyu/Vowel Count.cs
@@ -16,6 +16,14 @@ Link:
 
 // Solutions:
 // Solution 1:
+using System;
+using System.Linq;
+
+public static class Kata
+{
+    public static int GetVowelCount(string str) => 
+      str.Count(c => "aeiou".Contains(Char.ToLower(c)));
+}
 
 
 
