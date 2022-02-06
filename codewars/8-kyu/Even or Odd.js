aabdcac1eb9847e34edd14da45697a2fa705a82a
Adam Mohammed <66003360+Adam-Books@users.noreply.github.com>
2022-02-06 10:28:28 +0200

    Create Even or Odd.js

---
+++ codewars/8-kyu/Even or Odd.js
@@ -0,0 +1,14 @@
+/*
+Title:
+  Even or Odd
+  
+Description:
+  Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+Link:
+  https://www.codewars.com/kata/53da3dbb4a5168369a0000fe
+
+*/
+
+// Solutions:
+// Solution 1:
