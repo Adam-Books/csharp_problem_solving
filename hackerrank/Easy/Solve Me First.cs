b578f6953107ae6ec4256a18c93d9ba7cd940808
Adam Mohammed <66003360+Adam-Books@users.noreply.github.com>
2022-02-16 06:17:25 +0200

    Update Solve Me First.cs

--- hackerrank/Easy/Solve Me First.cs
+++ hackerrank/Easy/Solve Me First.cs
@@ -3,7 +3,37 @@ Title:
   Solve Me First
   
 Description:
-  
+  Complete the function solveMeFirst to compute the sum of two integers.
+
+  Example
+  a = 7
+  b = 3
+  Return 10.
+
+  Function Description
+
+  Complete the solveMeFirst function in the editor below.
+
+  solveMeFirst has the following parameters:
+
+  int a: the first value
+  int b: the second value
+  Returns
+  - int: the sum of  and 
+
+  Constraints
+  1 <= a, b <= 1000 
+
+  Sample Input
+
+  a = 2
+  b = 3
+  Sample Output
+
+  5
+  Explanation
+
+  2 + 3 = 5.
   
 Limk:
   https://www.hackerrank.com/challenges/solve-me-first/problem?isFullScreen=true
@@ -11,3 +41,6 @@ Limk:
 */
 
 // Solutions:
+
+
+
