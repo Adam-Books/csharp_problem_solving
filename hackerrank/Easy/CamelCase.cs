ff9b8bdb47dacde1ad1953a54c67ac07c448d6d4
Adam Mohammed <66003360+Adam-Books@users.noreply.github.com>
2022-02-25 16:21:20 +0200

    Update CamelCase.cs

--- hackerrank/Easy/CamelCase.cs
+++ hackerrank/Easy/CamelCase.cs
@@ -2,7 +2,50 @@
 Title:
   CamelCase
   
-  
+Description:
+  There is a sequence of words in CamelCase as a string of letters, , having the following properties:
+
+  It is a concatenation of one or more words consisting of English letters.
+  All letters in the first word are lowercase.
+  For each of the subsequent words, the first letter is uppercase and rest of the letters are lowercase.
+  Given , determine the number of words in .
+
+  Example
+
+  There are  words in the string: 'one', 'Two', 'Three'.
+
+  Function Description
+
+  Complete the camelcase function in the editor below.
+
+  camelcase has the following parameter(s):
+
+  string s: the string to analyze
+  Returns
+
+  int: the number of words in 
+  Input Format
+
+  A single line containing string .
+
+  Constraints
+
+  Sample Input
+
+  saveChangesInTheEditor
+  Sample Output
+
+  5
+  Explanation
+
+  String  contains five words:
+
+  save
+  Changes
+  In
+  The
+  Editor
+  Need help? Try this problem first to get familiar with HackerRank environment.
   
 Link:
   https://www.hackerrank.com/challenges/camelcase/problem?isFullScreen=true
