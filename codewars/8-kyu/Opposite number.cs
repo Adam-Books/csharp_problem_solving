df05d5873d348951f58aa86fdb9fc8840fd76dfe
Adam Mohammed <66003360+Adam-Books@users.noreply.github.com>
2022-03-14 20:27:54 +0200

    Opposite number - C# Solution

--- codewars/8-kyu/Opposite number.cs
+++ codewars/8-kyu/Opposite number.cs
@@ -24,6 +24,19 @@ public class Kata
         public static int Opposite(int n) => -n;
     }
 
+// Solution 2:
+using System;
+
+public class Kata
+    {
+        public  static int Opposite(int number)
+        {
+           if (number<0){ number = Math.Abs(number);}
+           else{number = number -number -number;}
+           return number;    
+        }
+    }
+
 
 
 
