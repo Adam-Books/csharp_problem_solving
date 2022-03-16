70207d6e5f8586597113c2880ab92dd4625d0d48
Adam Mohammed <66003360+Adam-Books@users.noreply.github.com>
2022-03-16 18:41:27 +0200

    Update Convert a Number to a String!.cs

--- codewars/8-kyu/Convert a Number to a String!.cs
+++ codewars/8-kyu/Convert a Number to a String!.cs
@@ -2,7 +2,14 @@
 Title:
   Convert a Number to a String!
   
-  
+Description:
+  We need a function that can transform a number into a string.
+
+  What ways of achieving this do you know?
+
+  Examples:
+  123 --> "123"
+  999 --> "999"
   
 Link:
   https://www.codewars.com/kata/5265326f5fda8eb1160004c8
