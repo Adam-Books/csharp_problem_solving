9237f5a8c25b49978e46b5bd11bb4e04cd21f43f
Adam Mohammed <66003360+Adam-Books@users.noreply.github.com>
2022-03-19 22:50:39 +0200

    Update Reversed Strings.cs

--- codewars/8-kyu/Reversed Strings.cs
+++ codewars/8-kyu/Reversed Strings.cs
@@ -2,6 +2,12 @@
 Title:
   Reversed Strings
   
+Description:
+  Complete the solution so that it reverses the string passed into it.
+
+  'world'  =>  'dlrow'
+  'word'   =>  'drow'
+  
 Link:
   https://www.codewars.com/kata/5168bb5dfe9a00b126000018
   
