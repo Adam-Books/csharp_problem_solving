0a5eb2fd22a028d63385d7354b71f55c51e4367e
Adam Mohammed <66003360+Adam-Books@users.noreply.github.com>
2022-03-23 20:02:50 +0200

    Update Convert boolean values to strings 'Yes' or 'No'.cs

--- codewars/8-kyu/Convert boolean values to strings 'Yes' or 'No'.cs
+++ codewars/8-kyu/Convert boolean values to strings 'Yes' or 'No'.cs
@@ -1,5 +1,8 @@
 /*
 Title:
   Convert boolean values to strings 'Yes' or 'No'.
+  
+Description:
+  Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
 
 */
