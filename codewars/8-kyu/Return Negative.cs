6927cc345a879837fb0902ca3336d771817d92e0
Adam Mohammed <66003360+Adam-Books@users.noreply.github.com>
2022-04-05 23:44:31 +0200

    Update Return Negative.cs

--- codewars/8-kyu/Return Negative.cs
+++ codewars/8-kyu/Return Negative.cs
@@ -2,5 +2,20 @@
 Title:
   Return Negative
 
+Description:
+  In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+  Examples
+  Kata.MakeNegative(1);  // return -1
+  Kata.MakeNegative(-5); // return -5
+  Kata.MakeNegative(0);  // return 0
+  Notes
+  The number can be negative already, in which case no change is required.
+  Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
+  FUNDAMENTALSNUMBERS
+  
+Link:
+  https://www.codewars.com/kata/55685cd7ad70877c23000102
+
 */
 
