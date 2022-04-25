8e14232c94d9c2d3932c840189362b93be1cb371
Adam Mohammed <66003360+Adam-Books@users.noreply.github.com>
2022-04-25 13:14:50 +0200

    Update String repeat.cs

--- codewars/8-kyu/String repeat.cs
+++ codewars/8-kyu/String repeat.cs
@@ -62,5 +62,16 @@ namespace Solution{
 }
 
 // Solution 4:
+using System.Linq;
 
+namespace Solution
+{
+  public static class Program
+  {
+    public static string repeatStr(int n, string s)
+    {
+      return string.Concat(Enumerable.Repeat(s, n));
+    }
+  }
+}
 
