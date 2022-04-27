60a42d5b57de504e1e95f8d7bd0a2653899d6d61
Adam Mohammed <66003360+Adam-Books@users.noreply.github.com>
2022-04-27 07:14:50 +0200

    String repeat - C# Solution

--- codewars/8-kyu/String repeat.cs
+++ codewars/8-kyu/String repeat.cs
@@ -84,6 +84,16 @@ class Program
 }
 
 //Solution 6:
-
+namespace Solution
+{
+  public static class Program
+  {
+    public static string repeatStr(int n, string s)
+    {
+      if(n==1) return s;
+      return repeatStr(n-1,s)+s;
+    }
+  }
+}
 
 
