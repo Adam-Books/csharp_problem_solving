141c289da97c38a3e1e9675a65337dcc0d54dab1
Adam Mohammed <66003360+Adam-Books@users.noreply.github.com>
2022-05-30 07:35:12 +0200

    Update Bin to Decimal.cs

--- codewars/8-kyu/Bin to Decimal.cs
+++ codewars/8-kyu/Bin to Decimal.cs
@@ -2,6 +2,9 @@
 Title:
   Bin to Decimal
 
+Description:
+  Complete the function which converts a binary number (given as a string) to a decimal number.
+
 Link:
   https://www.codewars.com/kata/57a5c31ce298a7e6b7000334
 
