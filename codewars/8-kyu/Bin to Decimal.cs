0a9ff3149bf0b4176a8fae8293056b5cd5fc12ab
Adam Mohammed <66003360+Adam-Books@users.noreply.github.com>
2022-06-01 19:26:32 +0200

    Update Bin to Decimal.cs

--- codewars/8-kyu/Bin to Decimal.cs
+++ codewars/8-kyu/Bin to Decimal.cs
@@ -13,6 +13,17 @@ Link:
 // Solutions:
 
 // Solution 1:
+using System;
+
+namespace Solution
+{
+  public static class Program
+  {
+    public static int binToDec(string s) => Convert.ToInt32(s, 2);
+  }
+}
+
+
 
 
 
