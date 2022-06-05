9c228e79ee6360a789c171984c51fe24d47f54d3
Adam Mohammed <66003360+Adam-Books@users.noreply.github.com>
2022-06-06 00:22:09 +0200

    Create Name on billboard.cs

---
+++ codewars/8-kyu/Name on billboard.cs
@@ -0,0 +1,14 @@
+
+
+
+
+// Solutions:
+
+// Solution 1:
+class Kata {
+  public static double Billboard(string s, double price = 30) {
+        double n = 0;
+        for (int i = 0; i < s.Length; i++) n += price;
+        return n;
+  }
+}
