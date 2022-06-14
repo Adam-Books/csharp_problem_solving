2125cad672f177b6e344e4f75ebe5fa3892c5c73
Adam Mohammed <66003360+Adam-Books@users.noreply.github.com>
2022-06-14 04:00:33 +0200

    Update Name on billboard.cs

--- codewars/8-kyu/Name on billboard.cs
+++ codewars/8-kyu/Name on billboard.cs
@@ -34,6 +34,16 @@ public class Kata {
 }
 
 // Solution 3:
+using System.Linq;
+
+public class Kata
+{
+  public static double Billboard(string name, double price = 30)
+  {
+    return name.Sum(_ => price);
+  }
+}
+
 
 
 
