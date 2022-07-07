1a839fcc00dd4cbf531aabbc0e10dbd00140f10a
Adam Mohammed <66003360+Adam-Books@users.noreply.github.com>
2022-07-07 20:57:37 +0200

    Update Pillars.cs

--- codewars/8-kyu/Pillars.cs
+++ codewars/8-kyu/Pillars.cs
@@ -18,3 +18,18 @@ Link:
 */
 
 // Solutions:
+
+class Kata
+{
+ public static int Pillars(int numPill, int dist, int width) => 
+   numPill == 1 ? 0 : ((numPill - 2) * width) + (((numPill - 1) * dist) * 100);
+}
+
+
+
+
+
+
+
+
+
